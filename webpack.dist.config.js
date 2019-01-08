b176fd70efe960358b2c47cf601d006ba5525282
jesseluo <luo_sj@163.com>
2019-01-09 00:30:51 +0800

    fix webpack.dist.config.js

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,45 +1,74 @@
 var path = require('path')
 var webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: './src/components/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    // publicPath: '/dist/',
-    filename: 'vm_markdown.min.js',
-    library: 'vm-markdown',
-    libraryTarget: 'umd'
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(woff|svg|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  }
+    entry: './src/components/index.js',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        // publicPath: '/dist/',
+        filename: 'vm_markdown.min.js',
+        library: 'vm-markdown',
+        libraryTarget: 'umd'
+    },
+    optimization: {
+        minimize: true
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                use: {
+                    loader: 'vue-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]?[hash]'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|svg|eot|ttf|otf)(\?.*)?$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]?[hash]'
+                    }
+                }]
+            }
+        ]
+    }
 }
